f3a2ee0323aac9502cb99f766e9189d7096800f4
sharleen <sharleen-quinn@live.nmit,ac.nz>
2021-03-15 12:49:06 +1300

    first commit

---
+++ src/Weekfour.js
@@ -0,0 +1,20 @@
+let country ="New Zealand"
+let city = "Nelson"
+let postalCode = "7010"
+
+console.log(country);
+console.log(city);
+console.log(postalCode);
+
+
+let isIsland = true
+let language
+
+console.log(typeof isIsland)
+console.log(typeof language)
+
+const String = 9
+
+
+
+
