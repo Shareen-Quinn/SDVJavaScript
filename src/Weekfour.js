178f60c0ad92fda4ae132f1d1ac52b3707cf98e1
sharleen <sharleen-quinn@live.nmit,ac.nz>
2021-03-15 13:59:00 +1300

    added new code

--- src/Weekfour.js
+++ src/Weekfour.js
@@ -8,12 +8,58 @@ console.log(postalCode);
 
 
 let isIsland = true
-let language
+let language // Will come back at a later time and use this language when i know what is spoken.
 
 console.log(typeof isIsland)
 console.log(typeof language)
 
-const String = 9
+language ="english"
+console.log(language)
+
+let number = 10
+console.log(number)
+
+console.log(number / 2)
+console.log(number >6)
+console.log(number <2)
+
+const address = country + " " + city + " " + postalCode 
+console.log(address)
+
+let string = `My address is ${country}, ${city} and ${postalCode}.`
+console.log(string)
+
+let x = 10
+
+if (x < 0) {
+
+console.log ('Sharleen')  // if and else statement example 
+
+} else {
+    console.log ('its false')
+}
+
+let S = 69
+if (S > 100) {
+console.log('No') // if and else statement example
+} else {
+    console.log('Error')
+}
+
+    let s = 69
+    if (s < 100){
+        console.log('yes') // if and then statement example
+    } 
+else { console.log('Error')}
+
+x = 10
+if (typeof x === 'number') {
+    console.log('correct')
+} else {
+    console.log('error')
+} 
+
+
 
 
 
